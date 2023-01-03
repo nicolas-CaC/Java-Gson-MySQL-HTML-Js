9166d4a80b3ecadd9e6485fc4f8b8e2795cfe977
nicolas-CaC <nicolas.machicado@bue.edu.ar>
2023-01-03 14:27:05 -0300

    refact: imports

--- src/main/java/controllers/ProductosController.java
+++ src/main/java/controllers/ProductosController.java
@@ -1,18 +1,18 @@
 package controllers;
 
-import com.google.gson.Gson;
+import Errores.Errores;
+import models.Productos;
+import java.util.Scanner;
+import models.ProductosDao;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.util.Scanner;
+import com.google.gson.Gson;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import models.Productos;
-import models.ProductosDao;
-import Errores.Errores;
 
 @WebServlet(name = "ProductosController", urlPatterns = {"/api"})
 public class ProductosController extends HttpServlet {
